73366a74441a15a758150be87d0490a4aeff9e3e
Aaron  Robinson <aaronrobinson@Aarons-MacBook-Pro.local>
2025-03-20 22:44:54 -0400

    minimum header component showing

---
+++ src/app/config/navigation.ts
@@ -0,0 +1,20 @@
+import { MainNavItem } from "../types/navigation";
+
+export const mainNavItems: MainNavItem[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Discover",
+    href: "/discover",
+  },
+  {
+    label: "My Journal",
+    href: "/journal",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+];
