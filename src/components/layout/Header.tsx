5747c106b0720037fb894dc44dfb0faaf1f01663
Aaron  Robinson <aaronrobinson@Aarons-MacBook-Pro.local>
2025-03-22 09:03:50 -0400

    css changes mostly

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { mainNavItems } from "@/app/config/navigation";
 import { MainNavItem } from "@/app/types/navigation";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 interface HeaderProps {
     navItems?: MainNavItem[]
@@ -8,7 +9,7 @@ interface HeaderProps {
 
 export function Header({ navItems = mainNavItems }: HeaderProps) {
  return (
-    <header className="fixed top-0 z-40 w-full bg-background px-8">
+    <header className="fixed top-0 z-40 w-full bg-background px-6 lg:px-12">
         <div className="flex h-20 items-center justify-between">
         <div className="">
           <Link href="/" className="">
@@ -18,12 +19,15 @@ export function Header({ navItems = mainNavItems }: HeaderProps) {
           </Link>
         </div>
 
-        <nav className="hidden lg:flex items-center space-x-10 text-lg font-medium tracking-tight">
+        <nav className="hidden lg:flex items-center space-x-6 text-lg font-medium tracking-tight">
             {navItems.map((item) => (
                 <Link key={item.href} href={item.href} className="hover:-translate-y-1 transition-all duration-500">
                     {item.label}
                 </Link>
             ))}
+            <Link href="/login">
+                <Button size='lg' className="text-lg">Login</Button>
+            </Link>
         </nav>
         </div>
     </header>
